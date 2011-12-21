c22867e29be34a31aaa3af476f5454a570d75de5
Elliot Winard <elliot.winard@frogdesign.com>
2011-12-21 16:05:38 -0500

    implement 'say' message handler

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-var irc = require('irc');
+var irc = require('irc')
+  , say = require ('say');
 
 var IRCSERVER = 'irc.freenode.net';
-var BOTNAME = 'EWBot';
+var BOTNAME = 'ElliotTheRobot';
 var CHANNELNAME = '#EWBot';
 
 var client = new irc.Client(IRCSERVER, BOTNAME, {
   channels: [CHANNELNAME],
 });
 
-function say(s) {
+// 'say' on IRC
+function isay(s) {
   client.say(CHANNELNAME, s);
 }
 
 var actions = {
   help: {
     help: 'display general help',
-    action: function ( args ) {
-      say('TODO:help');
+    action: function ( ) {
+      isay('TODO:help');
     }
   },
   
   say: {
     help: '"speak" on the server',
-    action: function ( args) {
-      say('TODO:speak');
+    action: function ( message ) {
+      isay('I am going to say "' + message + '"');
+      say.speak("Alex", message, function() {
+        isay("okay. i said it.")
+      });
     }
   }
 };
@@ -41,13 +46,16 @@ client.addListener('message', function (from, to, message) {
     if (tokens[0] == BOTNAME) {
       var action = tokens[1];
       if (actions[action]) {
-        client.say(CHANNELNAME, 'I will now execute action '+action);
+        client.say(CHANNELNAME, 'I hear you, man.');
+        var m = tokens.slice(2).join(' ');
+        actions[action].action(m);
         messageHandled = true;
       }
     }
     
     if (!messageHandled) {
-      client.say(CHANNELNAME, 'Hello, '+ from + '!');
+      isay('Hello, '+ from + '!');
+      // say.speak("Alex", "I do not know how to "+action);      
     }
   } else {
     // for now, ignore messages not for me
